58a14fefc4e252b97611973ad45a8f8500832aa8
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-09-09 10:23:53 -0300

    Update script.js

--- CSS_Advanced-main/script.js
+++ CSS_Advanced-main/script.js
@@ -4,13 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const lampImage = document.getElementById('lampImage');
     const pausePlayButton = document.getElementById('pausePlayButton');
 
-    // brilho input
     function updateBrightness() {
         const brightnessValue = brightnessRange.value;
         brightnessPercentage.textContent = `${brightnessValue}%`;
         brightnessPercentage.style.color = brightnessValue >= 90 ? '#000000' : '#ffffff';
         brightnessRange.classList.toggle('low', brightnessValue < 50);
-
         const progressColor = brightnessValue < 50 ? '#363636' : '#ffffff';
         brightnessRange.style.background = `linear-gradient(to right, ${progressColor} ${brightnessValue}%, #d3d3d3 ${brightnessValue}%)`;
 
@@ -18,11 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
             lampImage.style.filter = `brightness(${brightnessValue}%)`;
         }
     }
-
     brightnessRange.addEventListener('input', updateBrightness);
     updateBrightness();
 
-    // Adiciona o evento de clique ao botão de pausa/play
     function togglePausePlay() {
         if (pausePlayButton.src.includes('pause--v1.png')) {
             pausePlayButton.src = 'https://img.icons8.com/ios-glyphs/50/play.png';
